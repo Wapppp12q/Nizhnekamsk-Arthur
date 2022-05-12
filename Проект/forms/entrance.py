3b34605bde26720349cb66618ffa1210418c5288
Wapppp12q <96076172+Wapppp12q@users.noreply.github.com>
2022-05-12 22:37:26 +0300

    Delete Проект directory

--- Проект/forms/entrance.py
+++
@@ -1,9 +0,0 @@
-from .registerform import *
-
-
-class Entrance(FlaskForm):
-    email = EmailField('Почта:', validators=[DataRequired()])
-    password = PasswordField('Пароль:', validators=[DataRequired()])
-    rec = SubmitField('Забыли пароль?')
-    submit = SubmitField('Войти')
-    reg = SubmitField('Регистрация')
--- Проект/forms/page.py
+++
@@ -1,7 +0,0 @@
-from .registerform import *
-from wtforms import SubmitField
-
-
-class PageForm(FlaskForm):
-    status = StringField()
-    submit = SubmitField('Изменить фотографию')
