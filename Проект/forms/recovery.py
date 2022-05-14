9cac967866d0192ffe7fb9ccf55056c9cdbe2b74
Wapppp12q <96076172+Wapppp12q@users.noreply.github.com>
2022-05-14 16:01:27 +0300

    Delete Проект directory

--- Проект/forms/recovery.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import EmailField, SubmitField
-
-
-class Recovery(FlaskForm):
-    email = EmailField('Введите почту, на которую зарегистрирован аккаунт:')
-    submit = SubmitField('Отправить код')
