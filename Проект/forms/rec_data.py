447db382924d4665672b4668ff74c3d5b7526a59
Wapppp12q <96076172+Wapppp12q@users.noreply.github.com>
2022-05-16 14:47:14 +0300

    Delete Проект directory

--- Проект/forms/rec_data.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import PasswordField, SubmitField
-
-
-class RDataForm(FlaskForm):
-    password = PasswordField('Пароль')
-    pass_exam = PasswordField('Поторите пароль')
-    submit = SubmitField('Сохранить пароль')
--- Проект/forms/registerform.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, EmailField
-
-
-class RegisterForm(FlaskForm):
-    email = EmailField('Введите вашу почту')
-    submit = SubmitField('Отправить код')
-    entrance = SubmitField('Уже зарегистрированы?')
